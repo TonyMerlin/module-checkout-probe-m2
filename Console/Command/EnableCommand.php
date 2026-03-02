ac9ccf884b297d2556deddbb29906a8aa31c7a9c
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2026-03-02 22:55:07 +0000

    Update EnableCommand.php

--- Console/Command/EnableCommand.php
+++ Console/Command/EnableCommand.php
@@ -3,9 +3,9 @@ declare(strict_types=1);
 
 namespace Merlin\CheckoutProbe\Console\Command;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\App\Config\Storage\WriterInterface;
 use Magento\Framework\App\State;
-use Magento\Store\Model\ScopeInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -22,14 +22,21 @@ class EnableCommand extends Command
     protected function configure(): void
     {
         $this->setName('merlin:checkoutprobe:enable')
-            ->setDescription('Enable Merlin Checkout Probe (website scope)');
+            ->setDescription('Enable Merlin Checkout Probe (default scope)');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
+   protected function execute(InputInterface $input, OutputInterface $output): int
+{
         try { $this->state->setAreaCode('adminhtml'); } catch (\Throwable) {}
-        $this->writer->save('merlin_checkoutprobe/general/enabled', '1', ScopeInterface::SCOPE_DEFAULT, 0);
+
+        $this->writer->save(
+            'merlin_checkoutprobe/general/enabled',
+            '1',
+            \Magento\Framework\App\Config\ScopeConfigInterface::SCOPE_TYPE_DEFAULT,
+            0
+    );
+
         $output->writeln('<info>Enabled.</info>');
-        return Command::SUCCESS;
+        return 0;
     }
 }
