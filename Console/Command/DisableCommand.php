1e66ae97eeb7bf349ea7588cb2eb532bb3a272dd
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2026-03-02 21:39:43 +0000

    Create DisableCommand.php

---
+++ Console/Command/DisableCommand.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace Merlin\CheckoutProbe\Console\Command;
+
+use Magento\Framework\App\Config\Storage\WriterInterface;
+use Magento\Framework\App\State;
+use Magento\Store\Model\ScopeInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DisableCommand extends Command
+{
+    public function __construct(
+        private readonly WriterInterface $writer,
+        private readonly State $state
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->setName('merlin:checkoutprobe:disable')
+            ->setDescription('Disable Merlin Checkout Probe (website scope)');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        try { $this->state->setAreaCode('adminhtml'); } catch (\Throwable) {}
+        $this->writer->save('merlin_checkoutprobe/general/enabled', '0', ScopeInterface::SCOPE_DEFAULT, 0);
+        $output->writeln('<info>Disabled.</info>');
+        return Command::SUCCESS;
+    }
+}
